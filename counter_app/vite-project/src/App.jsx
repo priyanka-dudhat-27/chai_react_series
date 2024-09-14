b56da2777e637bbbb5a2e0bfecb74a742067d8eb
priyanka-dudhat-27 <pdudhat27@gmail.com>
2024-09-14 17:29:36 +0530

    first commit

---
+++ counter_app/vite-project/src/App.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable no-const-assign */
+/* eslint-disable no-undef */
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  let [counter,setCounter]=useState(5)
+
+  const handleAdd=()=>{
+    counter=counter+1
+    setCounter(counter)
+  }
+
+  const handleRemove=()=>{
+    counter=counter-1
+    setCounter(counter)
+  }
+  return (
+    <>
+      <h2>Counter</h2>
+      <h3>{counter}</h3>
+      <button onClick={handleAdd}>Increment</button>
+      <button onClick={handleRemove}>Decrement</button>
+    </>
+  )
+}
+
+export default App
